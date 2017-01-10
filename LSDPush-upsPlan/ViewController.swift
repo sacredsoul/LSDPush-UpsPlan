11312d8db2619af55128199c7196ba6dc2c806fb
SacredSoul <LSD@liusididemac-mini.local>
2017-01-10 08:52:15 +0800

    Initial Commit

---
+++ LSDPush-upsPlan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LSDPush-upsPlan
+//
+//  Created by LSD on 2017/1/10.
+//  Copyright © 2017年 LSD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
